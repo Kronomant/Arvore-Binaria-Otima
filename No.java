66fdccc6cd457e1e0814af0cb1cf3161bfb68c8c
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-05-08 22:52:57 -0300

    fix: correção dos erros de comparação das classes

--- No.java
+++ No.java
@@ -1,15 +1,43 @@
 
-class Element {
+class Element implements Comparable {
     public int valor;
     public int frequencia;
+
+    public Element(int valor, int frequencia) {
+        this.valor = valor;
+        this.frequencia = frequencia;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(int frequencia) {
+        this.frequencia = frequencia;
+    }
 }
 
 class No {
-    public Element elemento; 
-    public No esq, dir;  
+    public Element elemento;
+    public No esq, dir;
 
     /**
      * Construtor da classe.
+     * 
      * @param elemento Conteudo do no.
      */
     public No(Element elemento) {
@@ -18,13 +46,38 @@ class No {
 
     /**
      * Construtor da classe.
+     * 
      * @param elemento Conteudo do no.
-     * @param esq No da esquerda.
-     * @param dir No da direita.
+     * @param esq      No da esquerda.
+     * @param dir      No da direita.
      */
     public No(Element elemento, No esq, No dir) {
         this.elemento = elemento;
         this.esq = esq;
+        this.dir = dir;
+    }
+
+    public Element getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Element elemento) {
+        this.elemento = elemento;
+    }
+
+    public No getEsq() {
+        return esq;
+    }
+
+    public void setEsq(No esq) {
+        this.esq = esq;
+    }
+
+    public No getDir() {
+        return dir;
+    }
+
+    public void setDir(No dir) {
         this.dir = dir;
     }
 }
