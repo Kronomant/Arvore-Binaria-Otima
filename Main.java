e5e2e36101ea8a759ce793b82c736f3d3fad4a42
Kronomant <milardlucas@gmail.com>
2022-05-16 22:04:07 -0300

    #feat: optimalSearchTree

--- Main.java
+++ Main.java
@@ -1,22 +1,47 @@
-import java.io.BufferedReader;
-
-import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader buffRead = new BufferedReader(new FileReader("file.txt"));
-		String linha = "";
-		String[] values;
-		while (true) {
-			if (linha != null) {
-				values = linha.split("-");
-				System.out.println(values[0]);
-				
+    
+    static int optimalSearchTree(int keys[], int freq[], int n) {
 
-			} else
-				break;
-			linha = buffRead.readLine();
-		}
-		buffRead.close();
+        int cost[][] = new int[n + 1][n + 1];
+        
+        for (int i = 0; i < n; i++)
+            cost[i][i] = freq[i];
+       
+        for (int L = 2; L <= n; L++) {
+            for (int i = 0; i <= n - L + 1; i++) {
+                int j = i + L - 1;
+                cost[i][j] = Integer.MAX_VALUE;
+ 
+                for (int r = i; r <= j; r++) {
+                    int c = ((r > i) ? cost[i][r - 1] : 0)
+                            + ((r < j) ? cost[r + 1][j] : 0) + sum(freq, i, j);
+                    if (c < cost[i][j])
+                        cost[i][j] = c;
+                }
+            }
+        }
+        return cost[0][n - 1];
     }
+
+    static int sum(int freq[], int i, int j) {
+        int s = 0;
+        for (int k = i; k <= j; k++) {
+            if (k >= freq.length)
+                continue;
+            s += freq[k];
+        }
+        return s;
+    }
+ 
+    public static void main(String[] args) {
+         
+        int keys[] = { 10, 12, 20 };
+        int freq[] = { 34, 8, 50 };
+        int n = keys.length;
+        System.out.println("Custo para a árvore de busca ótima é: "
+                + optimalSearchTree(keys, freq, n));
+    }
+ 
+
 }
